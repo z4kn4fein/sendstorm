62718f20cfe62aad8f202dec6bb023de27f83855
Peter Csajtai <peter.csajtai@outlook.com>
2015-09-08 01:29:46 +0200

    Modified assembly info

--- src/sendstorm/Properties/AssemblyInfo.cs
+++ src/sendstorm/Properties/AssemblyInfo.cs
@@ -1,17 +1,15 @@
-﻿using System.Resources;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Reflection;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("sendstorm")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Sendstorm")]
+[assembly: AssemblyDescription("Portable messaging c# library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("sendstorm")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("Peter Csajtai")]
+[assembly: AssemblyProduct("Sendstorm")]
+[assembly: AssemblyCopyright("Copyright © Peter Csajtai 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
