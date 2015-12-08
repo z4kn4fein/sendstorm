fa518ce5ce53b6f8487b0f2834ce9a669228bdf8
Peter Csajtai <pcsajtai@3amlabs.net>
2015-12-08 14:25:33 +0100

    Modified README.md

--- coverage.ps1
+++ coverage.ps1
@@ -3,6 +3,6 @@ $coverallsPath = Join-Path $PSScriptRoot "src\packages\coveralls.io.1.3.4\tools\
 $testDllPath = Join-Path $PSScriptRoot "src\sendstorm.tests\bin\release\Sendstorm.Tests.dll"
 $vsTestPath = "c:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe"
 
-$arguments = "-register:user", "`"-filter:+[*]* -[sendstorm.tests*]*`"", "-target:$vsTestPath", "`"-targetargs:$testDllPath`"", "-output:coverage.xml"
+$arguments = "-register:user", "`"-filter:+[*]* -[sendstorm.tests]* `"", "-target:$vsTestPath", "`"-targetargs:$testDllPath`"", "-output:coverage.xml"
 . $openCoverPath $arguments
 . $coverallsPath --opencover coverage.xml
