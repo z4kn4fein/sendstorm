e1f54219c3606a784480d1d9d909fadfcf86c4c5
Peter Csajtai <pcsajtai@3amlabs.net>
2015-12-08 13:23:22 +0100

    Added test coverage calculation

---
+++ coverage.ps1
@@ -0,0 +1,15 @@
+﻿function Get-CurrentFilePath {
+    param([string]$fileName)
+    $PSScriptRoot = Split-Path -Parent -Path $MyInvocation.MyCommand.ScriptBlock.File
+	$targetPath = Join-Path -Path $PSScriptRoot -ChildPath $fileName 
+	return $targetPath
+}
+
+$currentPath = Get-CurrentFilePath
+$openCoverPath = Join-Path $currentPath "src\packages\OpenCover.4.6.166\tools\OpenCover.Console.exe"
+$coverallsPath = Join-Path $currentPath "src\packages\coveralls.io.1.3.4\tools\coveralls.net.exe"
+$testDllPath = Join-Path $currentPath "src\sendstorm.tests\bin\release\Sendstorm.Tests.dll"
+$vsTestPath = "c:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe"
+
+& $openCoverPath -register:user -filter:"+[*]* -[sendstorm.tests]*" -target:$vsTestPath -targetargs:"$testDllPath" -output:coverage.xml
+& $coverallsPath --opencover coverage.xml
