96ae194b2fcbe3ed8149f05e2af68afff2ee25f1
Peter Csajtai <pcsajtai@3amlabs.net>
2015-12-09 13:35:44 +0100

    Using moq

--- src/sendstorm.tests/MessagePublisherTests.cs
+++ src/sendstorm.tests/MessagePublisherTests.cs
@@ -24,9 +24,9 @@ namespace Sandstorm.Tests.MessagePublisherTests
         [ExpectedException(typeof(InvalidOperationException))]
         public void MessagePublisherTests_SubscribeDuplicatesTest()
         {
-            var reciever = new FakeIntMessageReciever();
-            publisher.Subscribe(reciever);
-            publisher.Subscribe(reciever);
+            var reciever = new Mock<IMessageReceiver<int>>();
+            publisher.Subscribe(reciever.Object);
+            publisher.Subscribe(reciever.Object);
         }
 
         [TestMethod]
@@ -38,98 +38,55 @@ namespace Sandstorm.Tests.MessagePublisherTests
         [TestMethod]
         public void MessagePublisherTests_SubscribePublishTest()
         {
-            var reciever = new FakeIntMessageReciever();
+            var reciever = new Mock<IMessageReceiver<int>>();
+            publisher.Subscribe(reciever.Object);
+            publisher.Broadcast(5);
+            reciever.Verify(rec => rec.Receive(5), Times.Once);
 
-            publisher.Subscribe(reciever);
-
-            var expectedResult = 5;
-
-            publisher.Broadcast(expectedResult);
-            Assert.AreEqual(expectedResult, reciever.Message);
-        }
-
-        [TestMethod]
-        public void MessagePublisherTests_MixedSubscriberPublishTest()
-        {
-            var reciever1 = new FakeIntMessageReciever();
-            var reciever2 = new FakeMixedMessageReciever();
-
-            publisher.Subscribe(reciever1);
-            publisher.Subscribe<string>(reciever2);
-            publisher.Subscribe<int>(reciever2);
-
-            var expectedResult = 5;
-            var expectedStringResult = "FakeMessage";
-
-            publisher.Broadcast(expectedResult);
-            publisher.Broadcast(expectedStringResult);
-
-            Assert.AreEqual(expectedResult, reciever1.Message);
-            Assert.AreEqual(expectedResult, reciever2.Message);
-            Assert.AreEqual(expectedStringResult, reciever2.StringMessage);
         }
 
         [TestMethod]
         public void MessagePublisherTests_UnSubscriberPublishTest()
         {
-            var reciever1 = new FakeIntMessageReciever();
-            var reciever2 = new FakeMixedMessageReciever();
+            var reciever = new Mock<IMessageReceiver<int>>();
+            publisher.Subscribe(reciever.Object);
+            publisher.UnSubscribe(reciever.Object);
+            publisher.Broadcast(5);
 
-            publisher.Subscribe(reciever1);
-            publisher.Subscribe<string>(reciever2);
-            publisher.Subscribe<int>(reciever2);
-
-            publisher.UnSubscribe(reciever1);
-            publisher.UnSubscribe<string>(reciever2);
-
-            var expectedResult = 5;
-            var expectedStringResult = "FakeMessage";
-
-            publisher.Broadcast(expectedResult);
-            publisher.Broadcast(expectedStringResult);
-
-            Assert.AreEqual(0, reciever1.Message);
-            Assert.AreEqual(expectedResult, reciever2.Message);
-            Assert.AreEqual(null, reciever2.StringMessage);
+            reciever.Verify(rec => rec.Receive(It.IsAny<int>()), Times.Never);
         }
 
         [TestMethod]
         public void MessagePublisherTests_SubscribeConditional()
         {
-            var reciever = new FakeConditionalMessageReciever();
+            var reciever = new Mock<IMessageReceiver<int>>();
+            publisher.Subscribe(reciever.Object, result => result == 5);
+            publisher.Broadcast(0);
 
-            publisher.Subscribe(reciever, result => result == 5);
-
-            var expectedResult = 0;
-
-            publisher.Broadcast(expectedResult);
-            Assert.AreEqual(expectedResult, reciever.Message);
+            reciever.Verify(rec => rec.Receive(It.IsAny<int>()), Times.Never);
         }
 
         [TestMethod]
         public void MessagePublisherTests_PublishOnBackgroundThread()
         {
             var completionSource = new TaskCompletionSource<int>();
-            var reciever = new BackgroundMessageReciever(completionSource);
-
-            publisher.Subscribe(reciever, executionTarget: ExecutionTarget.BackgroundThread);
-
-            var expectedResult = 5;
-
-            publisher.Broadcast(expectedResult);
+            var reciever = new Mock<IMessageReceiver<int>>();
+            reciever.Setup(rec => rec.Receive(It.IsAny<int>()))
+                .Callback((int message) => completionSource.SetResult(message))
+                .Verifiable();
+            publisher.Subscribe(reciever.Object, executionTarget: ExecutionTarget.BackgroundThread);
+            publisher.Broadcast(5);
             var result = completionSource.Task.Result;
-            Assert.AreEqual(expectedResult, result);
+            Assert.AreEqual(5, result);
         }
 
         [TestMethod]
         public void MessagePublisherTests_WeakReference()
         {
+            var reciever = new Mock<IMessageReceiver<int>>();
+            reciever.Setup(rec => rec.GetHashCode()).Returns(10);
 
-
-            var expectedResult = 5;
-            var reciever = new FakeHashCodeMessageReciever();
-
-            publisher.Subscribe(reciever);
+            publisher.Subscribe(reciever.Object);
 
             reciever = null;
             GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced, true);
@@ -137,98 +94,34 @@ namespace Sandstorm.Tests.MessagePublisherTests
             GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced, true);
 
             //Ensures broadcast to a collected object doesn't kill the observer
-            publisher.Broadcast(expectedResult);
+            publisher.Broadcast(5);
 
-            var newreciever = new FakeHashCodeMessageReciever();
-            publisher.Subscribe(newreciever);
+            var newreciever = new Mock<IMessageReceiver<int>>();
+            newreciever.Setup(rec => rec.GetHashCode()).Returns(10);
+            publisher.Subscribe(newreciever.Object);
 
-            publisher.Broadcast(expectedResult);
-            Assert.AreEqual(expectedResult, newreciever.Message);
+            publisher.Broadcast(5);
+
+            newreciever.Verify(rec => rec.Receive(5), Times.Once);
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentNullException))]
         public void MessagePublisherTests_PublishOnUiThread_FromNonUiThread()
         {
-            var reciever = new FakeIntMessageReciever();
-            publisher.Subscribe(reciever, executionTarget: ExecutionTarget.UiThread);
+            var reciever = new Mock<IMessageReceiver<int>>();
+            publisher.Subscribe(reciever.Object, executionTarget: ExecutionTarget.UiThread);
         }
 
         [TestMethod]
         public void SynchronizedSubscriptionTest()
         {
             var syncContext = new Mock<SynchronizationContext>();
-            var sub = new SynchronizedSubscription(new FakeIntMessageReciever(), null, null, syncContext.Object);
+            var sub = new SynchronizedSubscription(new Mock<IMessageReceiver<int>>().Object, null, null, syncContext.Object);
 
             sub.PublishMessage(5);
 
             syncContext.Verify(context => context.Post(It.IsAny<SendOrPostCallback>(), 5), Times.Once);
-        }
-
-        public class FakeIntMessageReciever : IMessageReceiver<int>
-        {
-            public int Message { get; set; }
-
-            public void Receive(int message)
-            {
-                this.Message = message;
-            }
-        }
-
-        public class FakeMixedMessageReciever : IMessageReceiver<int>, IMessageReceiver<string>
-        {
-            public int Message { get; set; }
-            public string StringMessage { get; set; }
-
-            public void Receive(int message)
-            {
-                this.Message = message;
-            }
-
-            public void Receive(string message)
-            {
-                this.StringMessage = message;
-            }
-        }
-
-        public class FakeHashCodeMessageReciever : IMessageReceiver<int>
-        {
-            public int Message { get; set; }
-
-            public void Receive(int message)
-            {
-                this.Message = message;
-            }
-
-            public override int GetHashCode()
-            {
-                return 10;
-            }
-        }
-
-        public class FakeConditionalMessageReciever : IMessageReceiver<int>
-        {
-            public int Message { get; set; }
-
-            public void Receive(int message)
-            {
-                this.Message = message;
-            }
-        }
-
-        public class BackgroundMessageReciever : IMessageReceiver<int>
-        {
-            private readonly TaskCompletionSource<int> completionSource;
-
-            public BackgroundMessageReciever(TaskCompletionSource<int> completionSource)
-            {
-                this.completionSource = completionSource;
-            }
-
-            public void Receive(int message)
-            {
-                this.completionSource.SetResult(message);
-            }
         }
     }
 }
