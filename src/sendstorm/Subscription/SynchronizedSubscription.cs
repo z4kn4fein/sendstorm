913705d8aae69d532b789ff01188b9547044f598
Peter Csajtai <pcsajtai@3amlabs.net>
2015-12-13 20:22:21 +0100

    Updated nuget packages

--- src/sendstorm/Subscription/SynchronizedSubscription.cs
+++ src/sendstorm/Subscription/SynchronizedSubscription.cs
@@ -11,7 +11,7 @@ namespace Sendstorm.Subscription
         public SynchronizedSubscription(object subscriber, object filterOrigin, MethodInfo filter, SynchronizationContext context)
             : base(subscriber, filterOrigin, filter)
         {
-            Shield.EnsureNotNull(() => context, "ExecutionTarget is set to UiThread but SynchornizationContext.Current is null, subscribe to this event from the Ui thread.");
+            Shield.EnsureNotNull(context, nameof(context), "ExecutionTarget is set to UiThread but SynchornizationContext.Current is null, subscribe to this event from the Ui thread.");
 
             this.context = context;
         }
